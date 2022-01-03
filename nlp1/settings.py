4f252793ba00a04853c3370481c2149951d6247a
Harsh <82361843+hackerharsh-77@users.noreply.github.com>
2022-01-03 13:08:10 +0530

    Add files via upload

--- nlp1/settings.py
+++ nlp1/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fz08h%-wa@aj71@@0nk(=1v8t1dhfrmqs3++h((a5pp0j)e-*k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
